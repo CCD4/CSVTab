c2ef9e1b20ca2971d55b919b639648d55d9ff9f5
yvonneg <yvonneg@CPYGNBW7>
2017-01-24 14:46:59 +0100

    csv tab

---
+++ CSV_Kata/CsvTab.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CSV_Kata
+{
+    internal class CsvTab
+    {
+        public static IEnumerable<string> Tabelliere(IEnumerable<string> CSV_zeilen)
+        {
+            var split = Split(CSV_zeilen);
+            var maxColumnLengths = MaxLength(split);
+            var result = FormatOutput(split, maxColumnLengths);
+            return result;
+        }
+
+        internal static IEnumerable<IEnumerable<string>> Split(IEnumerable<string> CSV_zeilen)
+        {
+            return CSV_zeilen.Select(zeile => zeile.Split(';'));
+        }
+
+        internal static int[] MaxLength(IEnumerable<IEnumerable<string>> splitLines)
+        {
+            var columnCounts = splitLines.First().Count();
+            var maxColumnLengths = new int[columnCounts];
+
+            foreach (var line in splitLines)
+            {
+                for (var i = 0; i < columnCounts; i++)
+                {
+                    var columnLength = line.ElementAt(i).Length;
+                    if (columnLength > maxColumnLengths[i])
+                        maxColumnLengths[i] = columnLength;
+                }
+            }
+            return maxColumnLengths;
+        }
+
+        internal static IEnumerable<string> FormatOutput(IEnumerable<IEnumerable<string>> splitLines,
+            int[] maxColumnLengths)
+        {
+            var columnCounts = splitLines.First().Count();
+            var result = new List<string>();
+
+            
+            foreach (var line in splitLines)
+            {
+                var formatedLine = string.Empty;
+                for (var i = 0; i < columnCounts; i++)
+                {
+                    var column = line.ElementAt(i);
+                    formatedLine += column.PadRight(maxColumnLengths[i]) + "|";
+                }
+                result.Add(formatedLine);
+                if (result.Count == 1)
+                    result.Add(FormatOutputHeader(maxColumnLengths));
+            }
+            return result;
+        }
+
+        internal static string FormatOutputHeader(int[] maxColumnLengths)
+        {
+            var result = string.Empty;
+            for (var i = 0; i < maxColumnLengths.Length; i++)
+            {
+                result = result + string.Empty.PadRight(maxColumnLengths[i], '-') + '+';
+            }
+            return result;
+        }
+    }
+}
---
+++ CSV_Kata/CsvTabTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CSV_Kata
+{
+    [TestFixture]
+    public class CsvTabTest
+    {
+        [Test]
+        public void FormatHeaderLineTest()
+        {
+            string expected = @"---------+---------+------------+-----+";
+            int[] maxColumnLengths = { 9, 9, 12, 5 };
+            
+            var actual = CsvTab.FormatOutputHeader(maxColumnLengths);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void MaxLengthTest()
+        {
+            string[][] input =
+            {
+                new[] {"Name", "Strasse", "Ort", "Alter"},
+                new[] {"Peter Pan", "Am Hang 5", "12345 Einsam", "42"}
+            };
+
+            var expected = CsvTab.MaxLength(input);
+            int[] actual = {9, 9, 12, 5};
+            Assert.AreEqual(expected, actual);
+        }
+
+        
+        [Test]
+        public void TestTabuliere()
+        {
+            string[] csvinput =
+            {
+                "Name;Strasse;Ort;Alter",
+                "Peter Pan;Am Hang 5;12345 Einsam;42",
+                "Maria Schmitz;Kölner Straße 45;50123 Köln;43",
+                "Paul Meier;Münchener Weg 1;87654 München;65"
+            };
+
+            string[] expected =
+            {
+                "Name         |Strasse         |Ort          |Alter|",
+                "-------------+----------------+-------------+-----+",
+                "Peter Pan    |Am Hang 5       |12345 Einsam |42   |",
+                "Maria Schmitz|Kölner Straße 45|50123 Köln   |43   |",
+                "Paul Meier   |Münchener Weg 1 |87654 München|65   |"
+            };
+
+            var actual = CsvTab.Tabelliere(csvinput);
+            foreach (var line in actual)
+            {
+                Console.WriteLine(line);
+            }
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void TestTabuliere2()
+        {
+            string[] csvinput =
+            {
+               "Name;Strasse;Ort;Alter"
+            };
+
+            string[] expected =
+            {
+               "Name|Strasse|Ort|Alter|",
+               "----+-------+---+-----+"
+
+            };
+
+            var actual = CsvTab.Tabelliere(csvinput);
+            foreach (var line in actual)
+            {
+                Console.WriteLine(line);
+            }
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
