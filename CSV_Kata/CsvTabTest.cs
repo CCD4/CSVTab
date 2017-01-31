75aa7f234e0cb1be1115d861e7c35ca5b112e1e2
svenk <svenk@CPKUEW10>
2017-01-31 08:05:22 +0100

    Gerüsttests raus

--- CSV_Kata/CsvTabTest.cs
+++ CSV_Kata/CsvTabTest.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace CSV_Kata
@@ -7,64 +6,6 @@ namespace CSV_Kata
     [TestFixture]
     public class CsvTabTest
     {
-        [Test]
-        public void TestFormatOutput()
-        {
-            string[] header = {"Name", "Strasse", "Ort", "Alter"};
-            string[][] input =
-            {
-                new[] {"Peter Pan", "Am Hang 5", "12345 Einsam", "42"}
-            };
-
-            var expected = new List<string>
-            {
-                "Name     |Strasse  |Ort         |Alter|",
-                "---------+---------+------------+-----+",
-                "Peter Pan|Am Hang 5|12345 Einsam|42   |"
-            };
-
-            int[] coloumLenghts = { 9, 9, 12, 5 };
-            var actual = CsvTab.FormatOutput(header, input, coloumLenghts);
-            Assert.AreEqual(expected, actual);
-        }
-
-        [Test]
-        public void TestFormatHeaderSeparator()
-        {
-            string expected = @"---------+---------+------------+-----+";
-            int[] maxColumnLengths = { 9, 9, 12, 5 };
-
-            var actual = CsvTab.FormatHeaderSeparator(maxColumnLengths);
-            Assert.AreEqual(expected, actual);
-        }
-
-        [Test]
-        public void TestMaxLength()
-        {
-            string[][] input =
-            {
-                new[] {"Name", "Strasse", "Ort", "Alter"},
-                new[] {"Peter Pan", "Am Hang 5", "12345 Einsam", "42"}
-            };
-
-            var expected = CsvTab.MaxLength(input);
-            int[] actual = { 9, 9, 12, 5 };
-            Assert.AreEqual(expected, actual);
-        }
-
-        [Test]
-        public void TestParse()
-        {
-            string[] csvinput =
-            {
-                "Name;Strasse;Ort;Alter"
-            };
-
-            string[][] expected = { new[] { "Name", "Strasse", "Ort", "Alter" } };
-            var actual = CsvTab.Parse(csvinput);
-            Assert.AreEqual(expected, actual);
-        }
-
         [Test]
         public void TestTabuliere()
         {
